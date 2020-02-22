f619e4344cf6d007345c5ccaab74cd56be8c1961
Filipe Morelli <morellitecinfo@gmail.com>
2020-02-22 17:43:34 -0300

    Refatoracao de StateScreen

--- lib/pages/states/states_screen.dart
+++ lib/pages/states/states_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:world_info/classes/Country.dart';
 
 class StateScreen extends StatefulWidget {
-  Country country;
+  final Country country;
 
   StateScreen({@required this.country});
 
@@ -11,13 +11,6 @@ class StateScreen extends StatefulWidget {
 }
 
 class _StateScreenState extends State<StateScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    print(widget.country.languages[0].toJson());
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,31 +26,106 @@ class _StateScreenState extends State<StateScreen> {
             mainAxisAlignment: MainAxisAlignment.start,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(left: 15, right: 15),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Text(
-                      widget.country.emoji,
-                      style: TextStyle(fontSize: 72),
-                    ),
-                    Text(widget.country.name),
-                    Text(widget.country.native),
-                    Text(widget.country.phone),
-                    Text(widget.country.code),
-                    Text(widget.country.languages
-                        .map((language) => language.name)
-                        .toList()
-                        .join(", ")),
-                  ],
-                ),
-              ),
+              buildContryInformation(),
+              Text("Olá mundo lindo"),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildContryInformation() {
+    return Container(
+      margin: EdgeInsets.all(15),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          buildFlagCountry(),
+          buildCountryDetails(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildFlagCountry() {
+    return Container(
+      margin: EdgeInsets.only(right: 15),
+      child: CircleAvatar(
+        radius: 45,
+        foregroundColor: Colors.indigo.shade400,
+        child: Text(
+          widget.country.emoji,
+          style: TextStyle(fontSize: 72),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCountryDetails() {
+    return Flexible(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(
+                "Native: ",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              Text(widget.country.native),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 5),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  "Phone Prefix: ",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text(widget.country.phone),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 5),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  "Country code: ",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text(widget.country.code),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 5),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  "Currency: ",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text(widget.country.currency),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 5),
+            child: Text(
+              widget.country.speakLanguages,
+              style: TextStyle(fontSize: 16),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+          ),
+        ],
       ),
     );
   }
