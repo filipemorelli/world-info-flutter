e39b45d43705f137be1d77aca5b38320e0b000b4
Filipe Morelli <morellitecinfo@gmail.com>
2020-02-22 11:01:58 -0300

    Ajustes para retirar exception inicial

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,18 @@ import 'package:world_info/globals/routes.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  MyApp() {
-    GraphqlBloc();
-  }
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    GraphqlBloc();
+
     return GraphQLProvider(
       client: GraphqlBloc.instance.client,
       child: CacheProvider(
         child: MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
-            primarySwatch: Colors.indigo,
-            splashColor: Colors.indigo
-          ),
+              primarySwatch: Colors.indigo, splashColor: Colors.indigo),
           navigatorKey: navigatorKey,
           debugShowCheckedModeBanner: debugShowCheckedModeBanner,
           showPerformanceOverlay: showSemanticsDebugger,
